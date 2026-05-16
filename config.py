0deecf8abb570797d72aaa3be54bd0fbed41cee2
vivekmantur <vivek.mantur@cognine.com>
2026-05-16 17:45:01 +0530

    config file data directory changes

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
-DATA_DIR = "/tmp/data"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
 
 os.makedirs(DATA_DIR, exist_ok=True)
 
 ORDERS_FILE = os.path.join(DATA_DIR, "orders.json")
+
 SUMMARY_FILE = os.path.join(DATA_DIR, "summary.json")
 
 SUMMARY_WINDOW_MINUTES = 5
