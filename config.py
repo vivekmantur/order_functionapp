c7ffcc217a49fe917e859af04839e856bba97e26
vivekmantur <vivek.mantur@cognine.com>
2026-05-16 17:30:13 +0530

    changed config file

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
+DATA_DIR = "/tmp/data"
 
-BASE_DIR = os.getcwd()
-DATA_DIR = os.path.join(BASE_DIR, "data")
+os.makedirs(DATA_DIR, exist_ok=True)
+
 ORDERS_FILE = os.path.join(DATA_DIR, "orders.json")
 SUMMARY_FILE = os.path.join(DATA_DIR, "summary.json")
+
 SUMMARY_WINDOW_MINUTES = 5
